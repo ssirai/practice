0a0e1b93335521f2bef94d9442576fe88e227e97
ssirai <gs.tn.bchld@gmail.com>
2014-07-15 11:38:16 +0900

    PE 5 shorten

--- exercises/project_euler/0-100/010/p005.jl
+++ exercises/project_euler/0-100/010/p005.jl
@@ -1,11 +1,1 @@
-N = 20
-function f(x)
-  ans = 1
-  while true
-    t = ans * x
-    t > N && return ans
-    ans = t
-  end
-end
-
-primes(N) |> x -> map(f, x) |> prod |> println
+foldl(lcm, 1:20) |> println
